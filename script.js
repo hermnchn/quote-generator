f5695bd766f62f95a363f74b3c436dac9961bb97
Herman <hermnchn@gmail.com>
2022-03-19 23:20:50 -0700

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get Quote From API
 async function getQuote() {
-    const apiUrl = 'https://type.fit/api/quotes';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
