30281f19448bb0c1d2961fb60ee2bf0fa26d00f8
Herman <hermnchn@gmail.com>
2022-03-20 00:12:18 -0700

    update

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
 // Get Quote From API
 async function getQuote() {
     loading();
@@ -12,12 +26,12 @@ async function getQuote() {
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
-        apiQuotes = await response.json();
+        const data = await response.json();
         // If Author is blank, add 'Unknown'
         if (data.quoteAuthor === '') {
             authorText.innerText = 'Unknown';
         } else {
-            authorText.innerText = data.quoteAuther;
+            authorText.innerText = data.quoteAuthor;
         }
         // Reduce font size for long quotes
         if (data.quoteText.length > 120) {
@@ -26,10 +40,9 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText;
-        //Stop Loader, Show Quote
+        // Stop Loader, Show Quote
         complete();
     } catch (error) {
-        // Catch Error Here
         getQuote();
     }
 }
@@ -41,6 +54,10 @@ function tweetQuote() {
     const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
     window.open(twitterUrl, '_blank');
 }
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
