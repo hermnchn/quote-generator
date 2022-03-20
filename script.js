4c78e580d0aa61ffe36804adeb0c40f6fafe2618
Herman <hermnchn@gmail.com>
2022-03-20 00:00:48 -0700

    update

--- script.js
+++ script.js
@@ -7,15 +7,39 @@ const loader = document.getElementById('loader');
 
 // Get Quote From API
 async function getQuote() {
+    loading();
     const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes);
+        // If Author is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuther;
+        }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        //Stop Loader, Show Quote
+        complete();
     } catch (error) {
         // Catch Error Here
+        getQuote();
     }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
 }
 
 // On Load
