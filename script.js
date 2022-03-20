0ed787e52372430881817a1f6904de3ff87de080
Herman <hermnchn@gmail.com>
2022-03-19 23:05:05 -0700

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+// Get Quote From API
+async function getQuote() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+
+    } catch (error) {
+        // Catch Error Here
+    }
+}
