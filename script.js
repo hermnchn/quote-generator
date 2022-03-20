0d523bf1fc9436b034419d0efb7d6f467a7751a0
Herman <hermnchn@gmail.com>
2022-03-19 23:44:26 -0700

    update

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 // Get Quote From API
 async function getQuote() {
+    const proxyUrl = 'https://whispering-tor-04671.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         apiQuotes = await response.json();
         console.log(apiQuotes);
     } catch (error) {
